f6bd09c016cb3851dc0db2a5b6d6035f249f6032
banoo Gargari <banoo.gargari@gmail.com>
2020-11-02 11:57:37 -0800

    create the homePage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HomePage = () => (
+    <>
+        <h1>Hello, Welcome to my blog</h1>
+        <p>
+            Welcome to my blog! Proin congue
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Laudantium vitae placeat non odit deserunt 
+            doloribus maxime rerum, optio quis nostrum porro fuga 
+            quae, excepturi facilis. Dignissimos, ea. Veritatis, 
+            soluta ad.
+        </p>
+
+        <p>
+            Welcome to my blog! Sahba Gargari
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Laudantium vitae placeat non odit deserunt 
+            doloribus maxime rerum, optio quis nostrum porro fuga 
+            quae, excepturi facilis. Dignissimos, ea. Veritatis, 
+            soluta ad.
+        </p>
+    </>
+);
+
+export default HomePage;
