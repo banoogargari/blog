900e7b78c89a110ed9fe5d06145e484479e41e3f
banoo Gargari <banoo.gargari@gmail.com>
2020-11-03 10:26:50 -0800

    modified the route path for each component

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <>
+        <h1>About me</h1>
+        <p>
+            Welcome to my blog! Proin congue
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Laudantium vitae placeat non odit deserunt 
+            doloribus maxime rerum, optio quis nostrum porro fuga 
+            quae, excepturi facilis. Dignissimos, ea. Veritatis, 
+            soluta ad.
+        </p>
+
+        <p>
+            Welcome to my blog! Sahba Gargari
+            Lorem ipsum dolor, sit amet consectetur adipisicing 
+            elit. Laudantium vitae placeat non odit deserunt 
+            doloribus maxime rerum, optio quis nostrum porro fuga 
+            quae, excepturi facilis. Dignissimos, ea. Veritatis, 
+            soluta ad.
+        </p>
+    </>
+);
+
+export default AboutPage;
