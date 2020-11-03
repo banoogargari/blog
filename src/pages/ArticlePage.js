4aaf04ee7f08e01e7afad737777668a589ee7b4c
banoo Gargari <banoo.gargari@gmail.com>
2020-11-03 12:49:25 -0800

    Created NotFoundPage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ArticlesList from '../components/ArticlesList'
+import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 
@@ -7,7 +8,7 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    if (!article) return <h1>Article does not exist!</h1>
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
