4449a229ab67c6a1d2bab857d126802402d259ad
banoo Gargari <banoo.gargari@gmail.com>
2020-12-04 11:09:13 -0800

    Adding an add comment

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 
 const ArticlePage = ({ match }) => {
@@ -34,6 +35,7 @@ const ArticlePage = ({ match }) => {
             <p key={key}>{paragraph}</p>
         ))}
         <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
         <h3>Other Articles: </h3>
         <ArticlesList articles={otherArticles} />
         </>
